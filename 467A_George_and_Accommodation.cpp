f33b3b2fb144464b4961d99b5aaf47eab7022358
Kazi Afnan Alam <yfkafnan2003@gmail.com>
2026-05-16 23:57:31 +0600

    16/5/2026

---
+++ 467A_George_and_Accommodation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int p[1000],q[1000];
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> p[i] >> q[i];
+        if(p[i] != q[i])
+        {
+            count++;
+        }
+    }
+    cout << count;
+
+}
